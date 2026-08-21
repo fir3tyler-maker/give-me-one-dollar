854fbb068c57749da40d2a00a1f933839473af40
fir3tyler-maker <fir3tyler@gmail.com>
2026-08-21 18:24:07 +0300

    Create counter.mjs

---
+++ netlify/functions/counter.mjs
@@ -0,0 +1,18 @@
+let total = 0;
+let people = 0;
+
+export default async (request) => {
+    return new Response(
+        JSON.stringify({
+            amount: total,
+            people: people
+        }),
+        {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+                "Cache-Control": "no-store"
+            }
+        }
+    );
+};
