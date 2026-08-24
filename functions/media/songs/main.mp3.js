ce691f67f59657427e32d3c987ca16ebf35ba3f3
fir3tyler-maker <fir3tyler@gmail.com>
2026-08-24 18:14:01 +0300

    Create main.mp3.js

---
+++ functions/media/songs/main.mp3.js
@@ -0,0 +1,18 @@
+```js
+export async function onRequestGet({ env }) {
+    const file = await env.MEDIA.get("songs/main.mp3");
+
+    if (!file) {
+        return new Response("songs/main.mp3 not found in R2", {
+            status: 404
+        });
+    }
+
+    return new Response(file.body, {
+        headers: {
+            "Content-Type": "audio/mpeg",
+            "Cache-Control": "public, max-age=3600"
+        }
+    });
+}
+```
